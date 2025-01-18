be9c1a50ef6f46e318c3cbcdd6bb06c3ebf65ce5
Sehej Brar <sehejpur@ualberta.ca>
2025-01-18 15:44:27 -0700

    basic gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
     return (
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,20 @@
+import gsap from 'gsap'
+import {useGSAP} from '@gsap/react'
 
 const Hero = () => {
 
+    useGSAP(() => {
+        gsap.fromTo("#intro", {opacity:0}, {y:200, opacity:100, duration:5, delay:0.25})
+    })
+
+    return (
+        <div className='flex justify-center text-black'>
+        <div id='intro' className='text-8xl mt-10 p-2'>
+            Hi, I'm Sehej Brar
+        </div>
+        <div>A Web Developer and Designer</div>
+        </div>
+    )
 }
 
 export default Hero;
