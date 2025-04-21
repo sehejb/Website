85746ccea2acd5ecf1f8c6647da7204ea4c20ed6
Sehej Brar <sehejpur@ualberta.ca>
2025-04-20 22:54:07 -0600

    gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,24 @@
 import gsap from 'gsap'
 import {useGSAP} from '@gsap/react'
 import { TextPlugin } from "gsap/TextPlugin";
+import { useRef } from 'react';
 
 gsap.registerPlugin(TextPlugin)
 
+function deleteChar(ref) {
+    let delChar = setInterval(() => {
+        const elem = ref?.current
+        const len = elem?.textContent.length
+        if (len) {elem.textContent = elem?.textContent.substring(0, len - 1)} else {clearInterval(delChar)}
+    }, 100); // delete one char every 0.5 s
+}
+
 const Hero = () => {
     const titles = ["Software Engineer", "Machine Learning Enthusiast", "Website Developer", "UI Designer"]
+    let titleRef = useRef(null)
+
+    // A timeline that manages the animations for the titles (repeats infintely, 1 sec delay)
+    let tlMaster = gsap.timeline({repeat:-1, repeatDelay: 2})
 
     useGSAP(() => {
         // Title animation
@@ -17,35 +30,32 @@ const Hero = () => {
         // Cursor animation
         // Fades out cursor, repeats the animation, reverses animation so it fades in, each fade lasts 2s, animation
         gsap.to("#cursor", {opacity:0, repeat:-1, yoyo:true, duration:0.5, ease:"power2.inOut"})
+
+        // For each title
+        titles.forEach((word) => {
+
+            // 3s to type the word, current word is animated
+            tlMaster.to(titleRef.current, {duration: 3, text: word, ease: gsap.SteppedEase})
+
+            // for the text to remain
+            tlMaster.to(titleRef.current, {duration: 2, text: word, ease: 'none'})
+
+            tlMaster.call(() => deleteChar(titleRef))
+            tlMaster.to({}, {duration: Math.round(word.length / 10)}) // delay of 0.1s per char
+        
+        // play the timeline
+        tlMaster.play()
+        })
     })
-
-    // A timeline that manages the animations for the titles (repeats infintely, 1 sec delay)
-    let tlMaster = gsap.timeline({repeat:-1, repeatDelay: 2})
-
-    // For each title
-    titles.forEach((word) => {
-
-        // 3s to type the word, current word is animated
-        tlMaster.to("#animated-text", {duration: 3, text: word, ease: gsap.SteppedEase})
-
-        // for the text to remain
-        tlMaster.to("#animated-text", {duration: 2, text: word, ease: 'none'})
-
-        // 2s to clear the word, 2s delay for the word to be seen
-        tlMaster.to("#animated-text", {duration: 2, text:"", ease: gsap.SteppedEase})
-    })
-
-    // play the timeline
-    tlMaster.play()
 
     return (
-        <div className='flex justify-center text-black'>
-            <div id='intro' className='text-8xl mt-10 p-2'>
+        <div className='flex flex-col justify-center text-black'>
+            <div id='intro' className='flex justify-center text-8xl mt-10 p-2'>
                 Hi, I'm Sehej Brar
             </div>
             
-            <div style={{"display": "flex"}}>
-                <div id="animated-text"></div>
+            <div className='flex justify-center text-6xl mt-10 p-2' style={{"display": "flex"}}>
+                <div ref={titleRef}></div>
                 <div id='cursor'>|</div>
             </div>
         </div>
