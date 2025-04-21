0ff3dbf26de3d007ea71c428724d87658b04764f
Sehej Brar <sehejpur@ualberta.ca>
2025-04-20 19:32:12 -0600

    gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,26 +14,29 @@ const Hero = () => {
         gsap.fromTo("#intro", {y: 50, opacity:0}, 
             {y:0, opacity:1, duration:2, delay:0.25})
 
-        // Curosr animation
+        // Cursor animation
         // Fades out cursor, repeats the animation, reverses animation so it fades in, each fade lasts 2s, animation
         gsap.to("#cursor", {opacity:0, repeat:-1, yoyo:true, duration:0.5, ease:"power2.inOut"})
     })
 
-    // A timeline that manages the animations for the titles (repeats infintely)
-    let tlMaster = gsap.timeline({repeat:-1})
+    // A timeline that manages the animations for the titles (repeats infintely, 1 sec delay)
+    let tlMaster = gsap.timeline({repeat:-1, repeatDelay: 2})
 
     // For each title
     titles.forEach((word) => {
-        // Timelines allow to sequence animations so they run after each other or control the entire sequence as a unit
-        let tlText = gsap.timeline({repeat: -1})
 
         // 3s to type the word, current word is animated
-        tlText.to("#animated-text", {duration: 3, text: word, ease:'none'})
+        tlMaster.to("#animated-text", {duration: 3, text: word, ease: gsap.SteppedEase})
 
-        // 3s to clear the word, 2s delay for the word to be seen
-        tlText.to("#animated-text", {duration: 3, text:"", delay: 2, ease:'none'})
-        tlMaster.add(tlText)
+        // for the text to remain
+        tlMaster.to("#animated-text", {duration: 2, text: word, ease: 'none'})
+
+        // 2s to clear the word, 2s delay for the word to be seen
+        tlMaster.to("#animated-text", {duration: 2, text:"", ease: gsap.SteppedEase})
     })
+
+    // play the timeline
+    tlMaster.play()
 
     return (
         <div className='flex justify-center text-black'>
@@ -41,7 +44,10 @@ const Hero = () => {
                 Hi, I'm Sehej Brar
             </div>
             
-            <div id='cursor'>|</div>
+            <div style={{"display": "flex"}}>
+                <div id="animated-text"></div>
+                <div id='cursor'>|</div>
+            </div>
         </div>
     )
 }
