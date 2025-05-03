cb367d1926e9bc9db8c9b1a473d68841264d7ca5
Sehej Brar <sehejpur@ualberta.ca>
2025-05-03 01:54:54 -0600

    starting about me

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,43 @@
+import gsap from 'gsap'
+import { useEffect, useRef } from 'react'
 
+let ballPos = []
+
+function Move(time, deltaTime, frame) {
+    ballPos.map((item) => {
+        item['x'] += 1
+        item['y'] += 1
+        gsap.set(item['el'], {x: item['x'], y: item['y']})
+    })
+}
+
+function Ball() {
+    const ballRef = useRef()
+
+    useEffect(() => {
+        const size = ballRef.current.getBoundingClientRect()
+
+        ballPos.push({x:size['x'], y:size['y'], el:ballRef.current})
+    }, [])
+
+    return(
+        <div id="ball" ref={ballRef} className="w-10 h-10 bg-white rounded-full"></div>
+    )
+}
 
 const Cards = () => {
+    const bowlRef = useRef()
+
+    useEffect(() => {
+        gsap.ticker.add(Move)
+        return () => gsap.ticker.remove(Move)
+    }, [])
+    
     return (
         <div className="flex h-full w-full justify-center">
             <div className="justify-center h-full w-full p-[1px] bg-white rounded-full">
-                <div className="justify-center h-full w-full bg-black rounded-full">
-                    Hello
+                <div ref={bowlRef} className="justify-center h-full w-full bg-black rounded-full overflow-hidden">
+                    <Ball/>
                 </div>
             </div>
         </div>
