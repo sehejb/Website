a27380a3fd1b1d4fa40c69b67f6d99d8b2ad1f7a
Sehej Brar <sehejpur@ualberta.ca>
2025-07-06 23:55:06 -0600

    adding icons and imports

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,32 @@
 import { useState } from "react"
+
 import { CiFolderOn } from "react-icons/ci";
+
+import { 
+  FaReact, FaAndroid, FaAngular, FaPython, FaJava, FaJsSquare, FaCss3, FaGitAlt 
+} from "react-icons/fa";
+
+import { VscVscode } from "react-icons/vsc";
+
+import { BiLogoVisualStudio } from "react-icons/bi";
+
+import { 
+  SiAndroidstudio, SiPytorch, SiJunit5, SiMongodb, SiPandas, SiNumpy, 
+  SiScikitlearn, SiC, SiNeo4J, SiAssemblyscript 
+} from "react-icons/si";
+
+import { IoLogoFirebase } from "react-icons/io5";
+
+import { RiTailwindCssFill } from "react-icons/ri";
+
+import { TbSdk } from "react-icons/tb";
+
+import { FcScatterPlot } from "react-icons/fc";
+
+import { DiSqllite } from "react-icons/di";
+
+import { PiFileCSharpDuotone } from "react-icons/pi";
+
 
 function Folder({data}) {
     const [enabled, setEnabled] = useState(false)
@@ -10,13 +37,16 @@ function Folder({data}) {
 
     return (
         <div>
-            <div onClick={Enable} className="flex flex-row items-center">
-                <CiFolderOn className="h-16 w-16 text-white m-5"></CiFolderOn>
-                <h1 className="text-white text-3xl items-center">{data.name}</h1>
+            <div onClick={Enable} className="relative flex items-center">
+                <CiFolderOn className="h-20 w-20 text-white m-5"></CiFolderOn>
+                <h1 className="text-white text-4xl">{data.name}</h1>
             </div>
 
-            {enabled && data.children.map(child => (
-                <h1 key={child.name} className="text-2xl text-white p-5">{child.name}</h1>
+            {enabled && data.children.map((child, i) => (
+                <div key={i} className="flex items-center">
+                    {child.icon && <child.icon key={child.icon} className="h-12 w-12 text-white ml-20"/>}
+                    <h1 key={child.name} className="text-3xl text-white pl-5">{child.name}</h1>
+                </div>    
             ))}
         </div>
     )
@@ -24,18 +54,58 @@ function Folder({data}) {
 
 const AboutMe = () => {
     const folders = [
-        {name: "Languages",
+    {
+        name: "Languages",
         type: "Folder",
-        children: [{name: "React", type: "File"}]},
-        
-        {name: "Libraries",
+        children: [
+            { name: "Java", type: "File", icon: FaJava },
+            { name: "Python", type: "File", icon: FaPython },
+            { name: "C", type: "File", icon: SiC },
+            { name: "SQL", type: "File", icon: DiSqllite },
+            { name: "JavaScript", type: "File", icon: FaJsSquare },
+            { name: "CSS", type: "File", icon: FaCss3 },
+            { name: "Neo4j", type: "File", icon: SiNeo4J },
+            { name: "C#", type: "File", icon: PiFileCSharpDuotone },
+            { name: "Assembly", type: "File", icon: SiAssemblyscript }
+        ]
+    },
+    {
+        name: "Libraries",
         type: "Folder",
-        children: [{name: "pandas", type: "File"}]},
-        
-        {name: "Frameworks",
+        children: [
+            { name: "pandas", type: "File", icon: SiPandas },
+            { name: "Firebase SDK", type: "File", icon: TbSdk },
+            { name: "MongoDB", type: "File", icon: SiMongodb },
+            { name: "NumPy", type: "File", icon: SiNumpy },
+            { name: "Scikit-learn", type: "File", icon: SiScikitlearn },
+            { name: "Matplotlib", type: "File", icon: FcScatterPlot }
+        ]
+    },
+    {
+        name: "Frameworks",
         type: "Folder",
-        children: [{name: "React", type: "File"}]},
-    ]
+        children: [
+            { name: "React", type: "File", icon: FaReact },
+            { name: "Firebase", type: "File", icon: IoLogoFirebase },
+            { name: "Android SDK", type: "File", icon: FaAndroid },
+            { name: "Angular", type: "File", icon: FaAngular },
+            { name: "Tailwind CSS", type: "File", icon: RiTailwindCssFill },
+            { name: "PyTorch", type: "File", icon: SiPytorch },
+            { name: "JUnit", type: "File", icon: SiJunit5 }
+        ]
+    },
+    {
+        name: "Developer Tools",
+        type: "Folder",
+        children: [
+            { name: "Git", type: "File", icon: FaGitAlt },
+            { name: "VS Code", type: "File", icon: VscVscode },
+            { name: "Visual Studio", type: "File", icon: BiLogoVisualStudio },
+            { name: "Android Studio", type: "File", icon: SiAndroidstudio }
+        ]
+    }
+]
+
     return (
         <div className="w-full h-full">
             <div className="w-2/5 h-full flex items-center justify-center">
