2c02545fcb373b1287706e8d8728a0ba60ad54c4
Sehej Brar <sehejpur@ualberta.ca>
2025-07-07 02:06:20 -0600

    adding the screen on the about me section

--- src/components/IDE.jsx
+++ src/components/IDE.jsx
@@ -7,12 +7,12 @@ const IDE = () => {
             <div className="flex bg-[#22252B] w-full h-[12vh] pt-2 rounded-t-xl">
                 <ul className="w-1/3 h-1/2 items-center justify-center flex">
                     {navBar.map((nav, i) => (
-                        <ul className="text-white text-2xl mx-5" key={i}>{nav}</ul>
+                        <ul className="text-[#838896] text-2xl mx-5" key={i}>{nav}</ul>
                     ))}
                 </ul>
 
                 <div className="w-2/5 h-1/2 flex items-center justify-center border-[#44474F] bg-[#2D3036] rounded-lg">
-                    <p className="text-3xl text-white">About Me</p>
+                    <p className="text-3xl text-[#838896]">About Me</p>
                 </div>
                 
             </div>
